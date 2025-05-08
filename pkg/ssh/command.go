e06e142427de27c4d2fd37871dd54454367fecd0
Aleksei Mavrin <al-mavrin@yandex-team.ru>
2025-05-08 17:20:42 +0300

    add Command and Tunnel to ssh

--- pkg/ssh/command.go
+++ pkg/ssh/command.go
@@ -1,6 +1,12 @@
 package ssh
 
-import "os/exec"
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+)
 
 type Host struct {
 	Address string
@@ -18,6 +24,33 @@ func New(address string) (Host, error) {
 	}, nil
 }
 
-func (h Host) Command(cmd string, sudo bool) (stdout, stderr string, e error) {
-	return "", "", nil
+func (h Host) Command(c string, sudo bool) (string, error) {
+	if c == "" {
+		return "", errors.New("cmd should not be empty")
+	}
+	cmdLine := strings.Fields(c)
+	args := []string{"-t", h.Address}
+	if sudo {
+		args = append(args, "sudo")
+	}
+	args = append(args, cmdLine...)
+	cmd := exec.Command(h.SSHPath, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	out := strings.TrimSpace(string(output))
+	return out, nil
+}
+
+func (h Host) Tunnel(local, remote int) (func(), error) {
+	portSpec := fmt.Sprintf("%d:localhost:%d", local, remote)
+	args := []string{"-L", portSpec, h.Address, "sleep", "infinity"}
+	cmd := exec.Command(h.SSHPath, args...)
+	err := cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(3 * time.Second)
+	return func() { cmd.Process.Kill() }, nil
 }
--- pkg/ssh/command_test.go
+++ pkg/ssh/command_test.go
@@ -1,10 +1,15 @@
 package ssh
 
 import (
+	"bufio"
+	"flag"
+	"net"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,10 +22,50 @@ func whichSSH(t *testing.T) string {
 	return p
 }
 
+var TestSSHServer string
+
+func TestMain(m *testing.M) {
+	flag.StringVar(&TestSSHServer, "server", "localhost", "user@server to use in test")
+	flag.Parse()
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
 func TestNew(t *testing.T) {
 	h, err := New("")
 	require.NoError(t, err)
 	sshPath := whichSSH(t)
 	require.NotEmpty(t, sshPath)
 	require.Equal(t, h.SSHPath, sshPath)
+}
+
+func TestSSHCommand(t *testing.T) {
+	h, err := New(TestSSHServer)
+	require.NoError(t, err)
+
+	out, err := h.Command("id -u", false)
+	require.NoError(t, err)
+	assert.Equal(t, out, "1001")
+
+	out, err = h.Command("id -u", true)
+	require.NoError(t, err)
+	assert.Equal(t, out, "0")
+}
+
+func TestSSHTunnel(t *testing.T) {
+	h, err := New(TestSSHServer)
+	disconnect, err := h.Tunnel(2022, 22)
+	defer disconnect()
+	require.NoError(t, err)
+	conn, err := net.Dial("tcp", "localhost:2022")
+	require.NoError(t, err)
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	resp, err := reader.ReadString('\n')
+	require.NoError(t, err)
+
+	assert.Contains(t, resp, "OpenSSH")
 }
