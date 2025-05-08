d3d753dc4daef45a9ffc511dcbc2a0bfd44464a9
Aleksei Mavrin <al-mavrin@yandex-team.ru>
2025-05-08 22:50:23 +0300

    ssh: add chat mode

--- pkg/ssh/command.go
+++ pkg/ssh/command.go
@@ -1,8 +1,10 @@
 package ssh
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"os/exec"
 	"strings"
 	"time"
@@ -11,6 +13,13 @@ import (
 type Host struct {
 	Address string
 	SSHPath string
+	Prompt  byte
+	StdIn   io.WriteCloser
+	StdOut  io.ReadCloser
+	StdErr  io.ReadCloser
+	Scanner *bufio.Scanner
+	Reader  *bufio.Reader
+	Cmd     *exec.Cmd
 }
 
 func New(address string) (Host, error) {
@@ -21,10 +30,15 @@ func New(address string) (Host, error) {
 	return Host{
 		Address: address,
 		SSHPath: path,
+		Prompt:  '$',
 	}, nil
 }
 
-func (h Host) Command(c string, sudo bool) (string, error) {
+func (h *Host) SetPrompt(p byte) {
+	h.Prompt = p
+}
+
+func (h *Host) Command(c string, sudo bool) (string, error) {
 	if c == "" {
 		return "", errors.New("cmd should not be empty")
 	}
@@ -43,7 +57,7 @@ func (h Host) Command(c string, sudo bool) (string, error) {
 	return out, nil
 }
 
-func (h Host) Tunnel(local, remote int) (func(), error) {
+func (h *Host) Tunnel(local, remote int) (func(), error) {
 	portSpec := fmt.Sprintf("%d:localhost:%d", local, remote)
 	args := []string{"-L", portSpec, h.Address, "sleep", "infinity"}
 	cmd := exec.Command(h.SSHPath, args...)
@@ -53,4 +67,57 @@ func (h Host) Tunnel(local, remote int) (func(), error) {
 	}
 	time.Sleep(3 * time.Second)
 	return func() { cmd.Process.Kill() }, nil
+}
+
+func (h *Host) StartChat() error {
+	args := []string{"-tt", h.Address}
+	cmd := exec.Command(h.SSHPath, args...)
+	stdIn, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	h.StdIn = stdIn
+	stdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	h.StdOut = stdOut
+	h.Scanner = bufio.NewScanner(h.StdOut)
+	h.Reader = bufio.NewReader(h.StdOut)
+	stdErr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+	h.StdErr = stdErr
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *Host) WaitFor(wait string, maxLines int) (string, error) {
+	ret := ""
+	for h.Scanner.Scan() {
+		line := h.Scanner.Text()
+		ret += line + "\n"
+		maxLines--
+		if maxLines < 0 {
+			return "", errors.New("max lines reached with no match")
+		}
+		if !strings.Contains(ret, wait) {
+			continue
+		}
+		return ret, nil
+	}
+	return ret, errors.New("no more input and not match")
+}
+
+func (h *Host) Send(what string) error {
+	_, err := io.WriteString(h.StdIn, what+"\n")
+	return err
+}
+
+func (h *Host) GetPrompt() (string, error) {
+	return h.Reader.ReadString(h.Prompt)
 }
