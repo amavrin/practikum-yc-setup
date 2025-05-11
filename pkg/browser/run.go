69b8daa8e6e872a05c10167b9414af5081803f29
Alexey Mavrin <amavrin@gmail.com>
2025-05-11 12:02:12 +0300

    open url in Windows

--- pkg/browser/run.go
+++ pkg/browser/run.go
@@ -4,26 +4,32 @@ import (
 	"errors"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
 func Open(url string) error {
 	var path string
 	var err error
+	var cmd *exec.Cmd
 	switch runtime.GOOS {
 	case "darwin":
 		path, err = exec.LookPath("open")
 		if err != nil {
 			return err
 		}
+		cmd = exec.Command(path, url)
 	case "linux":
 		path, err = exec.LookPath("xdg-open")
 		if err != nil {
 			return err
 		}
+		cmd = exec.Command(path, url)
+	case "windows":
+		winURL := strings.ReplaceAll(url, "&", "^&")
+		cmd = exec.Command("cmd", "/c", "start", "", winURL)
 	default:
 		return errors.New("unsupported OS")
 	}
 
-	cmd := exec.Command(path, url)
 	return cmd.Run()
 }
